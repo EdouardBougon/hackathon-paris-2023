e3c5aa9e7a7c8df5e97b55ef79006e9a26104476
tobou <npasquie@users.noreply.github.com>
2023-07-22 15:07:34 +0200

    fix deploy

--- services/contracts/script/Deploy.s.sol
+++ services/contracts/script/Deploy.s.sol
@@ -5,6 +5,8 @@ import "forge-std/Script.sol";
 
 import "../src/PositionDelegation.sol";
 
+contract RandomAddress {}
+
 contract Deploy is Script {
     function run() public {
         uint forkId = vm.createFork("https://polygon.llamarpc.com");
@@ -13,6 +15,6 @@ contract Deploy is Script {
         vm.broadcast();
 
         PositionDelegation factory = new PositionDelegation(0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619);
-        factory.getOrCreateSafe(address(this));
+        factory.getOrCreateSafe(address(new RandomAddress()));
     }
 }
