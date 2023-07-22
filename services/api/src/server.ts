3bb2c2cb2968b8610094ec5429bc268e3c4dd669
Edouard Bougon <15703023+EdouardBougon@users.noreply.github.com>
2023-07-23 00:35:22 +0200

    Fix Api

--- services/api/src/server.ts
+++ services/api/src/server.ts
@@ -12,9 +12,8 @@ import express from 'express';
 import { polygon } from 'viem/chains';
 
 const app: express.Express = express();
-const port: number = process.env.PORT !== undefined ? Number(process.env.PORT) : 8887;
-// const hostname: string = `http://localhost:${port}`;
-const hostname: string = 'https://position-api.onrender.com';
+const internalPort: number = process.env.PORT !== undefined ? Number(process.env.PORT) : 8887;
+const externalPort: number = process.env.PORT !== undefined ? 80 : internalPort;
 
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
@@ -49,7 +48,7 @@ app.get('/api/token/metadata', async (req: Request, res: Response): Promise<void
   const body: any = {
     name: `Delegated ${permission}`,
     description: `NFT ${tokenId} for the delegated position account ${walletAddress}`,
-    image: `${hostname}/api/token/metadata/image?tokenId${tokenId}`,
+    image: `${req.protocol}://${req.hostname}:${externalPort}/api/token/metadata/image?tokenId=${tokenId}`,
     external_url: `https://polygonscan.com/tx/${walletAddress}`,
     attributes: [
       {
@@ -90,12 +89,13 @@ app.get('/api/token/metadata/image', async (req: Request, res: Response): Promis
   const body: any =`
     <svg width="300" height="300" xmlns="http://www.w3.org/2000/svg">
       <rect width="300" height="300" x="0" y="0" fill="#1F2548" />
-      <a href="https://polygonscan.com/tx/${walletAddress}" target="_blank">
-        <text x="50%" y="20%" fill="white" text-decoration="underline" font-family="Arial, Helvetica, sans-serif" dominant-baseline="middle" text-anchor="middle" font-size="20px">Delegated Wallet ${tokenId}</text>
+      <a href="https://polygonscan.com/address/${walletAddress}#tokentxnsErc721" target="_blank">
+        <text x="50%" y="10%" fill="white" text-decoration="underline" font-family="Arial, Helvetica, sans-serif" dominant-baseline="middle" text-anchor="middle" font-size="20px">Delegated Wallet ${tokenId}</text>
       </a>
       <text x="50%" y="35%" fill="white" font-family="Arial, Helvetica, sans-serif" dominant-baseline="middle" text-anchor="middle" font-size="20px">Wallet: ${walletAddress?.slice(0, 4)}...${walletAddress?.slice(-4)}</text>
-      <text x="50%" y="35%" fill="white" font-family="Arial, Helvetica, sans-serif" dominant-baseline="middle" text-anchor="middle" font-size="20px">Permission: ${permission}</text>
-      <text x="50%" y="75%" fill="white" font-family="Arial, Helvetica, sans-serif" dominant-baseline="middle" text-anchor="middle" font-size="20px">Total Value: ${positionString} USD</text>
+      <text x="50%" y="45%" fill="white" font-family="Arial, Helvetica, sans-serif" dominant-baseline="middle" text-anchor="middle" font-size="20px">Permission: ${permission}</text>
+      <text x="50%" y="75%" fill="white" font-family="Arial, Helvetica, sans-serif" dominant-baseline="middle" text-anchor="middle" font-size="20px">Total Value:</text>
+      <text x="50%" y="85%" fill="white" font-family="Arial, Helvetica, sans-serif" dominant-baseline="middle" text-anchor="middle" font-size="20px">${positionString} USD</text>
     </svg>`;
 
   res.writeHead(200, {}).end(body)
@@ -129,8 +129,8 @@ async function getWalletPosition(tokenId: bigint): Promise<bigint | undefined> {
   }) 
 }
 
-app.listen(port, () => {
+app.listen(internalPort, () => {
   // eslint-disable-next-line no-console
-  console.log(`Dev server started on port ${port}`);
+  console.log(`Dev server started on port ${internalPort}`);
 });
 
