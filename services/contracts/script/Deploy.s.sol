61b10d4e88d55284c33eb46278a466d6b870d39b
tobou <npasquie@users.noreply.github.com>
2023-07-22 11:58:11 +0200

    update deploy

--- services/contracts/script/Deploy.s.sol
+++ services/contracts/script/Deploy.s.sol
@@ -3,6 +3,18 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
 
-contract Deploy is Script {
+import "../src/NftLoan.sol";
+import "../mock/NFT.sol";
 
+contract Deploy is Script {
+    function run() public {
+        vm.createFork("https://polygon.llamarpc.com");
+
+        PositionDelegation factory = new PositionDelegation();
+        NFT nft = new NFT();
+
+        vm.broadcast();
+
+        factory.createSafe(address(this), nft, 1, 1);
+    }
 }
