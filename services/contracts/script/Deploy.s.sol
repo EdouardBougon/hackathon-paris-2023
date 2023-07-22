1c855fb4d08173c303822d84e8e25b37bea3894c
tobou <npasquie@users.noreply.github.com>
2023-07-22 17:34:58 +0200

    fix deploy onchain

--- services/contracts/script/Deploy.s.sol
+++ services/contracts/script/Deploy.s.sol
@@ -12,7 +12,7 @@ contract Deploy is Script {
         uint forkId = vm.createFork("https://polygon.llamarpc.com");
         vm.selectFork(forkId);
 
-        vm.broadcast();
+        vm.startBroadcast();
 
         PositionDelegation factory = new PositionDelegation(0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619);
         factory.getOrCreateSafe(address(new RandomAddress()));
