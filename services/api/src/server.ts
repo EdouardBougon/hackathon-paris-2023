f35c8764fa80df7a01f704ed7a7a24acf346499a
Edouard Bougon <15703023+EdouardBougon@users.noreply.github.com>
2023-07-23 03:48:11 +0200

    fix error with multi contract

--- services/api/src/server.ts
+++ services/api/src/server.ts
@@ -34,72 +34,80 @@ app.get('/api/heartbeat', async (req: Request, res: Response): Promise<void> => 
 });
 
 app.get('/api/token/metadata', async (req: Request, res: Response): Promise<void> => {
-  if (req.query.tokenId === undefined) {
-    res.writeHead(400, {}).end('Missing tokenId query parameter');
-    return;
+  try {
+
+    if (req.query.tokenId === undefined) {
+      res.writeHead(400, {}).end('Missing tokenId query parameter');
+      return;
+    }
+  
+    const tokenId: bigint = BigInt(req.query.tokenId as string);
+    const permission: string = await isTokenIdOwnerOfWallet(BigInt(tokenId)) ?
+      'OWNER' :
+      'USER';
+    const walletAddress: string | undefined = await getWalletAddress(tokenId);
+    const position: bigint | undefined = await getWalletPosition(tokenId);
+    const positionString: string = formatUnits(position ?? 0n, 9);
+  
+    const body: any = {
+      name: `Delegated ${permission}`,
+      description: `NFT ${tokenId} for the delegated position account ${walletAddress}`,
+      image: `${protocol}://${req.hostname}:${externalPort}/api/token/metadata/image?tokenId=${tokenId}`,
+      external_url: `https://polygonscan.com/tx/${walletAddress}`,
+      attributes: [
+        {
+          trait_type: "Position",
+          value: positionString,
+        },
+        {
+          trait_type: "Wallet Address",
+          value: walletAddress,
+        },
+        {
+          trait_type: "Permission",
+          value: walletAddress,
+        },      
+      ]
+    };
+  
+    res.writeHead(200, {}).end(JSON.stringify(body))
+  } catch (error) {
+    res.writeHead(200, {}).end(JSON.stringify({}))
   }
 
-  const tokenId: bigint = BigInt(req.query.tokenId as string);
-  const permission: string = await isTokenIdOwnerOfWallet(BigInt(tokenId)) ?
-    'OWNER' :
-    'USER';
-  const walletAddress: string | undefined = await getWalletAddress(tokenId);
-  const position: bigint | undefined = await getWalletPosition(tokenId);
-  const positionString: string = formatUnits(position ?? 0n, 9);
-
-  const body: any = {
-    name: `Delegated ${permission}`,
-    description: `NFT ${tokenId} for the delegated position account ${walletAddress}`,
-    image: `${protocol}://${req.hostname}:${externalPort}/api/token/metadata/image?tokenId=${tokenId}`,
-    external_url: `https://polygonscan.com/tx/${walletAddress}`,
-    attributes: [
-      {
-        trait_type: "Position",
-        value: positionString,
-      },
-      {
-        trait_type: "Wallet Address",
-        value: walletAddress,
-      },
-      {
-        trait_type: "Permission",
-        value: walletAddress,
-      },      
-    ]
-  };
-
-  console.log(walletAddress, permission, position, positionString)
-
-  res.writeHead(200, {}).end(JSON.stringify(body))
 });
 
 app.get('/api/token/metadata/image', async (req: Request, res: Response): Promise<void> => {
-  if (req.query.tokenId === undefined) {
-    res.writeHead(400, {}).end('Missing tokenId query parameter');
-    return;
+  try {
+    if (req.query.tokenId === undefined) {
+      res.writeHead(400, {}).end('Missing tokenId query parameter');
+      return;
+    }
+  
+    const tokenId: bigint = BigInt(req.query.tokenId as string);
+    const walletAddress: string | undefined = await getWalletAddress(tokenId);
+    const position: bigint | undefined = await getWalletPosition(tokenId);
+    const positionString: string = formatUnits(position ?? 0n, 9);
+  
+    const permission: string = await isTokenIdOwnerOfWallet(tokenId) ?
+      'OWNER' :
+      'USER';
+  
+    const body: any =`<svg width="300" height="300" xmlns="http://www.w3.org/2000/svg">
+        <rect width="300" height="300" x="0" y="0" fill="white" />
+        <a href="https://polygonscan.com/address/${walletAddress}#tokentxnsErc721" target="_blank">
+          <text x="50%" y="10%" fill="#ED2647" text-decoration="underline" font-family="Arial, Helvetica, sans-serif" dominant-baseline="middle" text-anchor="middle" font-size="20px">Delegated Wallet ${tokenId}</text>
+        </a>
+        <text x="50%" y="35%" fill="#ED2647" font-family="Arial, Helvetica, sans-serif" dominant-baseline="middle" text-anchor="middle" font-size="20px">Wallet: ${walletAddress?.slice(0, 4)}...${walletAddress?.slice(-4)}</text>
+        <text x="50%" y="45%" fill="#ED2647" font-family="Arial, Helvetica, sans-serif" dominant-baseline="middle" text-anchor="middle" font-size="20px">Permission: ${permission}</text>
+        <text x="50%" y="75%" fill="#ED2647" font-family="Arial, Helvetica, sans-serif" dominant-baseline="middle" text-anchor="middle" font-size="20px">Total Value:</text>
+        <text x="50%" y="85%" fill="#ED2647" font-family="Arial, Helvetica, sans-serif" dominant-baseline="middle" text-anchor="middle" font-size="20px">${positionString} USD</text>
+      </svg>`;
+  
+    res.writeHead(200, {'Content-Type': 'image/svg+xml'}).end(body)
+  } catch (error) {
+    res.writeHead(200, {'Content-Type': 'image/svg+xml'}).end(`<svg width="300" height="300" xmlns="http://www.w3.org/2000/svg"></svg>`)
   }
-
-  const tokenId: bigint = BigInt(req.query.tokenId as string);
-  const walletAddress: string | undefined = await getWalletAddress(tokenId);
-  const position: bigint | undefined = await getWalletPosition(tokenId);
-  const positionString: string = formatUnits(position ?? 0n, 9);
-
-  const permission: string = await isTokenIdOwnerOfWallet(tokenId) ?
-    'OWNER' :
-    'USER';
-
-  const body: any =`<svg width="300" height="300" xmlns="http://www.w3.org/2000/svg">
-      <rect width="300" height="300" x="0" y="0" fill="white" />
-      <a href="https://polygonscan.com/address/${walletAddress}#tokentxnsErc721" target="_blank">
-        <text x="50%" y="10%" fill="#ED2647" text-decoration="underline" font-family="Arial, Helvetica, sans-serif" dominant-baseline="middle" text-anchor="middle" font-size="20px">Delegated Wallet ${tokenId}</text>
-      </a>
-      <text x="50%" y="35%" fill="#ED2647" font-family="Arial, Helvetica, sans-serif" dominant-baseline="middle" text-anchor="middle" font-size="20px">Wallet: ${walletAddress?.slice(0, 4)}...${walletAddress?.slice(-4)}</text>
-      <text x="50%" y="45%" fill="#ED2647" font-family="Arial, Helvetica, sans-serif" dominant-baseline="middle" text-anchor="middle" font-size="20px">Permission: ${permission}</text>
-      <text x="50%" y="75%" fill="#ED2647" font-family="Arial, Helvetica, sans-serif" dominant-baseline="middle" text-anchor="middle" font-size="20px">Total Value:</text>
-      <text x="50%" y="85%" fill="#ED2647" font-family="Arial, Helvetica, sans-serif" dominant-baseline="middle" text-anchor="middle" font-size="20px">${positionString} USD</text>
-    </svg>`;
-
-  res.writeHead(200, {'Content-Type': 'image/svg+xml'}).end(body)
 });
 
 
